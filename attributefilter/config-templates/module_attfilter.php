13b674daccb73961b70cc1462dd02e633bba3e59
Riccardo 'G. Valzorio <valzorio@cilea.it>
2012-05-04 15:27:08 +0200

    Initial version

---
+++ attributefilter/config-templates/module_attfilter.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * attributefilter:AttributeFilter
+ * 
+ * 
+ * 
+ */
+
+$config = array(
+
+'https://sso.cilea.it/simplesaml/module.php/saml/sp/metadata.php/saml2' => array('mail','eduPersonTargetedID'),
+
+);
